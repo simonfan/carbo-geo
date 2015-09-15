0f2513e151b274938af16eda9176e7c623188dd5
Leonardo "Alemax" Fonseca Ferreira <leualemax@gmail.com>
2015-09-15 19:35:14 -0300

    0.0.5 - add features and fixes

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Polymer component project
+_                                 
+| |                                
+___ __ _ _ __| |__   ___ ______ __ _  ___  ___  
+/ __/ _` | '__| '_ \ / _ \______/ _` |/ _ \/ _ \
+| (_| (_| | |  | |_) | (_) |    | (_| |  __/ (_) |
+\___\__,_|_|  |_.__/ \___/      \__, |\___|\___/
+                  __/ |           
+                 |___/            
 
-This is the base project for creating a polymer component for carbono.
-
-To develop, run `gulp develop` on the command line.
-To rename the component, run `gulp rename` on the command line.
+                 
--- src/carbo-geo.js
+++ src/carbo-geo.js
@@ -1,15 +1,30 @@
 (function () {
     var CarboGeo;
+
     Polymer({
         is: 'carbo-geo',
         properties: {
+          lang: {type: String, reflectToAttribute: true, value:'pt-br' },
+          position:{type: String, reflectToAttribute: true, value:'top' },
+          mensage:String,
           latitude: {type: Number, reflectToAttribute: true },
           longitude: {type: Number, reflectToAttribute: true },
-          display:{type:Boolean , reflectToAttribute:true, value:true},
+          display:{type:String , reflectToAttribute:true, value:'hide'},
+        },
+
+        status:{
+          'pt-br':{
+            loading:'Carregando Geolocalização ... ',
+            success:'Geolocalização: ',
+            error:'Ops! algo deu errado. '
+          }
         },
 
         ready: function () {
             document.addEventListener('deviceready',  this._whenCordovaReady(), false);
+            this.set('mensage', this.status[this.lang].loading );
+            this.classList.add(this.position);
+            this.classList.add(this.display);
             CarboGeo = this;
         },
 
@@ -26,13 +41,16 @@
          geolocationSuccess: function (location) {
            CarboGeo.set('latitude',location.coords.latitude);
            CarboGeo.set('longitude' , location.coords.longitude);
-           console.log(location);
+           CarboGeo.set('mensage', CarboGeo.status[CarboGeo.lang].success + ' ' + location.coords.latitude + '|' + location.coords.longitude);
+           CarboGeo.classList.add('success');
+           console.log('geolocation loaded: ' , location);
          },
          /**
           * start the watch for geolocation
           */
          geolocationError: function () {
-
+            CarboGeo.set('class','danger');
+            CarboGeo.classList.add('error');
          },
     });
 })();
